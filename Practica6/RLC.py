2ee9ee347e8d71ba6fcae7c0b9b8a251f0e1891a
kennethsyddall <kensyd_98@hotmail.com>
2021-06-05 12:07:54 -0300

    Data 5/6/21 mañana

--- Practica6/RLC.py
+++ Practica6/RLC.py
@@ -9,10 +9,9 @@ Created on Fri Jun  4 10:39:51 2021
 #%% Setup paths
 import os
 from scipy import fftpack
-os.chdir(r'/home/tian/Desktop/labo4/Grupo5/Practica5/Grupo5/RP')
-script_dir = os.path.abspath('/home/tian/Desktop/labo4/Grupo5/RP')
-project_dir =  os.path.dirname(os.path.abspath('/home/tian/Desktop/labo4/Grupo5/RP'))
-data_dir = os.path.join(project_dir, 'data')
+os.chdir(r'/home/publico/Desktop/Grupo 5/')
+script_dir = os.path.abspath('/home/publico/Desktop/Grupo 5/')
+data_dir = os.path.join(script_dir, 'data')
 
 
 from scipy import signal
@@ -23,7 +22,111 @@ from time import sleep,time
 import control_finn
 from control_finn import RedPitayaApp, reg_labels
 
-rp = RedPitayaApp('http://10.200.1.240/lock_in+pid_harmonic/?type=run', name='rp')
+rp = RedPitayaApp('http://rp-f061b6.local/lock_in+pid_harmonic_hf/?type=run', name='rp')
 #%%
 
+#frecuencia  = rp.set_freq( 10 )
+Vmod_out1   = rp.set_modulation_amplitud(2048, ch='out1')
+frecuencias =np.array( np.exp(np.linspace(np.log(1239),np.log(0.0001),20)),dtype='int')
 
+#print(f"Configuramos la frecuencia de modulación en {frecuencia} Hz")
+#print(f"A la salida out1 se le suma la modulación con {Vmod_out1} V de amplitud")
+
+#rp.set_lpf(16 , order=1 , line='ref' ) 
+#tau , orden = rp.get_lpf('ref')
+
+#print(f"El pasabajos para la democulación de X e Y es de orden {orden} y tiempo característico {tau} seg ")
+#print(f"La frecuencia de corte es fc={1/(tau*2*np.pi)} Hz ")
+
+
+#X_en_ints  = rp.lock.X_28 con esto adquiris directo los datos mas rapido
+#X, Y = rp.get_XY()
+X=[]
+Y=[]
+
+
+for i in range(10):
+
+    rp.set_lpf(20+i,order=2 , line='ref' ) 
+    print(20+i)
+    tau , orden = rp.get_lpf('ref')
+    X.append([])
+    Y.append([])
+    for f in frecuencias:
+        frec  = rp.set_freq( f )
+        sleep(1) 
+        x, y = rp.get_XY(units='int') / 8192
+        X[i].append(x)
+        Y[i].append(y)
+    X[i]=np.array(X[i])
+    Y[i]=np.array(Y[i])
+#%%
+for i in range(10):
+    plt.semilogx(frecuencias,np.sqrt(X[i]**2+Y[i]**2),'-',label='{}'.format(20 + i))
+    plt.xlabel('Frecuencias')
+    plt.ylabel('X')
+    plt.legend()
+#%%
+
+Vmod_out1   = rp.set_modulation_amplitud(4048, ch='out1')
+
+frecuencias =np.array( np.exp(np.linspace(np.log(1239),np.log(0.0001),100)),dtype='int')
+frecss=[]
+for i,f in enumerate(frecuencias):
+    if i<len(frecuencias)-1:
+        if frecuencias[i+1] != frecuencias[i]:
+            frecss.append(frecuencias[i])
+
+frecuencias=np.array(frecss)
+
+X=[]
+Y=[]
+for i in range(10):
+
+    rp.set_lpf(20+i,order=2 , line='ref' ) 
+    print(20+i)
+    tau , orden = rp.get_lpf('ref')
+    X.append([])
+    Y.append([])
+    for f in frecuencias:
+        frec  = rp.set_freq( f )
+        sleep(1) 
+        x, y = rp.get_XY(units='int') / 8192
+        X[i].append(x)
+        Y[i].append(y)
+    X[i]=np.array(X[i])
+    Y[i]=np.array(Y[i])
+
+#%%   
+for i in range(10):
+    plt.semilogx(frecuencias,X[i]**2+Y[i]**2,'-',label='{}'.format(20 + i))
+    plt.xlabel('Frecuencias')
+    plt.ylabel('X')
+    plt.legend()
+    
+#%%
+for i in range(10):
+    df = pd.DataFrame({'frecs': frecuencias, 'X': X[i], 'Y': Y[i]})
+    df.to_csv(r'data/FCandFRECS/{}.csv'.format(i))
+
+#%%
+Amps=np.linspace(0,8190,200)
+rp.set_lpf(22,order=2 , line='ref' ) 
+frec  = rp.set_freq( 38 )
+tau , orden = rp.get_lpf('ref')
+sleep(1) 
+Xa=[]
+Ya=[]
+for a in Amps:
+    Vmod_out1   = rp.set_modulation_amplitud(a, ch='out1')
+    sleep(1)
+    x, y = rp.get_XY(units='int') / 8192
+    Xa.append(x)
+    Ya.append(y)
+Xa=np.array(Xa)
+Ya=np.array(Ya)
+#%%
+plt.plot(Amps,Xa)
+df = pd.DataFrame({'amp': Amps, 'X': Xa, 'Y': Ya})
+df.to_csv(r'data/Amps.csv')
+    
